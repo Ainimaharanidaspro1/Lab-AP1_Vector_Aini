38db65708fdfc8e4af4e65e3d3436c431fc870bc
AiniMaharani <114594860+Ainimaharanidaspro1@users.noreply.github.com>
2023-05-09 08:39:33 -0600

    Add files via upload

--- main (3).cpp
+++ main (3).cpp
@@ -4,28 +4,25 @@
 
 using namespace std;
 
-int main()
-{
-    vector<int> nums = {1, 2, 3, 0, 4, 5,0,0}; // vector awal
+// Fungsi untuk memindahkan angka 0 ke kiri dan mengurutkan angka dari terkecil ke terbesar
+void sortNonZero(vector<int>& nums) {
     int count = 0;
-
-    // Memindahkan angka 0 ke kiri
     for (int i = 0; i < nums.size(); i++) {
         if (nums[i] != 0) {
             swap(nums[i], nums[count]);
             count++;
         }
     }
+    sort(nums.begin(), nums.end()); //mengurutkan angka dari terkecil ke terbesar
+}
 
-    // Mengurutkan angka dari terkecil ke terbesar
-    sort(nums.begin(), nums.end());
-
-    // Menampilkan vektor hasil
+int main() {
+    vector<int> nums = {1, 2, 3, 0, 4, 5, 0, 0}; // vector awal
+    sortNonZero(nums); // panggil fungsi untuk memindahkan angka 0 ke kiri dan mengurutkan angka dari terkecil ke terbesar
     for (int i = 0; i < nums.size(); i++) {
-        cout << nums[i] << " ";
+        cout << nums[i] << " "; //menampilkan vektor hasil
     }
     cout << endl;
-
     return 0;
 }
 
